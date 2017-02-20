98aa9bdaac0a0241e90937e1a27c1490933ea503
Paul O Donnell <podonne5@gmail.com>
2017-02-20 15:04:06 +0000

    TKCustomMap plugin

---
+++ XamarinMaps/XamarinMapsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinMaps
+{
+    public partial class XamarinMapsPage : ContentPage
+    {
+        public XamarinMapsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinMaps/iOS/AppDelegate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using TK.CustomMap.iOSUnified;
+
+namespace XamarinMaps.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            // Code for starting up the Xamarin Test Cloud Agent
+#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start();
+#endif
+
+            TKCustomMapRenderer.InitMapRenderer();
+            NativePlacesApi.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
